638a30687009c654ad5f8c8a1a337609979d70a3
Fernando Guedes - META <fernando_guedes@beirario.com.br>
2019-12-26 13:20:59 -0200

    concluindo a parte funcional da calculadora

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import './Button.css';
 
 export default props => {
-    let classes = 'button'
-    classes += props.operation ? 'operation' : ''
-    classes += props.double ? 'double' : ''
-    classes += props.triple ? 'triple' : ''
+    let classes = 'button '
+    classes += props.operation ? 'operation' : '';
+    classes += props.double ? 'double' : '';
+    classes += props.triple ? 'triple' : '';
 
     return (
-        <button className={classes}>
+        <button className={classes} 
+            onClick={e => props.click && props.click(props.label)} >
             {props.label}
         </button>
     )
